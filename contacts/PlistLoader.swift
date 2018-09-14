2a9973b207a72408ccd5e31a0d8630365c99d319
aananya <astream.h@outlook.com>
2018-09-14 18:27:29 +0530

    Initial commit

---
+++ contacts/PlistLoader.swift
@@ -0,0 +1,35 @@
+//
+//  PlistLoader.swift
+//  contacts
+//
+//  Created by Aananya on 14/09/18.
+//  Copyright © 2018 Aananya. All rights reserved.
+//
+
+import Foundation
+enum PlistError: Error {
+    case invalidResource
+    case parsingFailure
+}
+
+class PlistLoader {
+    static func array(fromFile name: String, ofType type: String) throws -> [[String: String]] {
+        guard let path = Bundle.main.path(forResource: name, ofType: type) else {
+            throw PlistError.invalidResource
+        }
+        
+        guard let array = NSArray(contentsOfFile: path) as? [[String: String]] else {
+            throw PlistError.parsingFailure
+        }
+        
+        return array
+    }
+}
+
+class ContactsSource {
+    static var contacts: [Contact] {
+        let data = try! PlistLoader.array(fromFile: "ContactsDB", ofType: "plist")
+        return data.flatMap { Contact(dictionary: $0) }
+    }
+    
+}
